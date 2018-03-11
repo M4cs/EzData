743f0e44377fa58f85be183aae48bbb09b27675d
M4cs <maxlikescs@gmail.com>
2018-03-11 00:46:45 -0500

    Initial FIle Load

---
+++ EzDataModule.h
@@ -0,0 +1,6 @@
+#import <ControlCenterUIKit/CCUIToggleModule.h>
+#import <CoreTelephony/CTCellularData.h>
+
+@interface EzDataModule: CCUIToggleModule
+@property (nonatomic, assign, readwrite) BOOL ezdata;
+@end
---
+++ EzDataModule.m
@@ -0,0 +1,41 @@
+#import "EzDataModule.h"
+
+@interface CTCellularData ()
++ (BOOL)CTCellularRestrictedState;
++ (void)CTCellularRestrictedState:(BOOL)arg1;
+@end
+
+@interface UIImage ()
++ (UIImage *)imageNamed:(NSString *)name inBundle:(NSBundle *)bundle;
+@end
+
+@implementation EzData
+- (UIImage *)iconGlyph {
+	return [UIImage imageNamed:@"Icon" inBundle:[NSBundle bundleForClass:[self class]]];
+}
+
+- (UIColor *)selectedColor {
+	return [UIColor purpleColor];
+}
+
+- (BOOL)isSelected {
+	if ([CTCellularData CTCellularRestrictedState]) {
+		self.ezdata = TRUE;
+	} else {
+		self.ezdata = FALSE;
+	}
+	return self.ezdata;
+}
+
+- (void)setSelected:(BOOL)selected {
+	if (CTCellularData CTCellularRestrictedState]) {
+		[CTCellularData CTCellularRestrictedState]):FALSE];
+		selected = FALSE;
+	} else {
+		[CTCellularData CTCellularRestrictedState]):TRUE];
+		selected = TRUE;
+	}
+	self.ezlocation = selected;
+	[super refreshState];
+}
+@end
